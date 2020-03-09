2c763292a05b35516335aa0d5ff6d1f88a30fe47
Deyvison J Paula <deyvisonjp@id.uff.br>
2020-03-09 09:45:57 -0300

    Fucoes add - listar - buscar prontas

--- index.js
+++ index.js
@@ -1,5 +1,85 @@
 // Base a ser utilizada
-const alunosDaEscola=[{nome:"Henrique",notas:[],cursos:[],faltas:5},{nome:"Edson",notas:[],cursos:[],faltas:2},{nome:"Bruno",notas:[10,9.8,9.6],cursos:[],faltas:0},{nome:"Guilherme",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"Full Stack",dataMatricula:new Date}],faltas:0},{nome:"Carlos",notas:[],cursos:[],faltas:0},{nome:"Lucca",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"UX",dataMatricula:new Date}],faltas:0}];
+const alunosDaEscola = [{
+        nome: "Henrique",
+        notas: [],
+        cursos: [],
+        faltas: 5
+    },
+    {
+        nome: "Edson",
+        notas: [],
+        cursos: [],
+        faltas: 2
+    },
+    {
+        nome: "Bruno",
+        notas: [10, 9.8, 9.6],
+        cursos: [],
+        faltas: 0
+    },
+    {
+        nome: "Guilherme",
+        notas: [10, 9.8, 9.6],
+        cursos: [{
+            nomeDoCurso: "Full Stack",
+            dataMatricula: new Date
+        }],
+        faltas: 0
+    },
+    {
+        nome: "Carlos",
+        notas: [],
+        cursos: [],
+        faltas: 0
+    },
+    {
+        nome: "Lucca",
+        notas: [10, 9.8, 9.6],
+        cursos: [{
+            nomeDoCurso: "UX",
+            dataMatricula: new Date
+        }],
+        faltas: 1
+    }
+];
 
 
 // implementação
+
+// Adicionar um novo aluno
+function adicionarAluno(nome) {
+    alunosDaEscola.push({nome, notas: [],cursos: [], faltas: 0});
+    return `Aluno ${nome} adicionado com sucesso!`;
+}
+
+//Listar Alunos
+const listarAlunos = () => {
+    alunosDaEscola.forEach((aluno, indice) => {
+        console.log(
+                "_".repeat(15) + "[" + indice + "]" + "_".repeat(15) + "\n Nome: " +
+                aluno.nome + "\n Notas: " +
+                aluno.notas + "\n Faltas: " +
+                aluno.faltas) +
+                aluno.cursos.forEach((curso) => console.log(" Curso: " + curso.nomeDoCurso + " - Data: " +
+                    curso.dataMatricula.getDate() + "/" + (curso.dataMatricula.getMonth()+1) + "/" + curso.dataMatricula.getUTCFullYear()));
+    })
+}
+
+//Buscar por nome de aluno
+function buscarAluno(nome){
+    let result = alunosDaEscola.filter((elemento) => {
+        return elemento.nome.indexOf(nome) !== -1;
+    });
+    if (result.length >= 1) {
+        console.log("Aluno " + nome + " encontrado:") 
+        return result; 
+    } else {
+        return `Aluno [${nome}] não Cadastrado!`
+    }   
+}
+
+
+
+//console.log(adicionarAluno("Deyvison"));
+//console.log(listarAlunos());
+//console.log(buscarAluno("Henrique"))
