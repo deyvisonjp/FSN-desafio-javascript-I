8500c8579f50a65cec0eb976aa7f55f5b31c4c5d
Deyvison J Paula <deyvisonjp@id.uff.br>
2020-03-09 12:57:42 -0300

    Funcao cadastrar aluno em curso

--- index.js
+++ index.js
@@ -1,43 +1,26 @@
 // Base a ser utilizada
-const alunosDaEscola = [{
-        nome: "Henrique",
-        notas: [],
-        cursos: [],
-        faltas: 5
+const alunosDaEscola = [
+    {
+        nome: "Henrique", notas: [], cursos: [], faltas: 5 
     },
     {
-        nome: "Edson",
-        notas: [],
-        cursos: [],
-        faltas: 2
+        nome: "Edson", notas: [], cursos: [], faltas: 2
     },
     {
-        nome: "Bruno",
-        notas: [10, 9.8, 9.6],
-        cursos: [],
+        nome: "Bruno", notas: [10, 9.8, 9.6], cursos: [], faltas: 0
+    },
+    {
+        nome: "Guilherme", notas: [10, 9.8, 9.6],
+        cursos: [{ nomeDoCurso: "Full Stack", dataMatricula: new Date}],
         faltas: 0
     },
     {
-        nome: "Guilherme",
-        notas: [10, 9.8, 9.6],
-        cursos: [{
-            nomeDoCurso: "Full Stack",
-            dataMatricula: new Date
-        }],
-        faltas: 0
-    },
-    {
-        nome: "Carlos",
-        notas: [],
-        cursos: [],
-        faltas: 0
+        nome: "Carlos", notas: [], cursos: [], faltas: 0
     },
     {
         nome: "Lucca",
         notas: [10, 9.8, 9.6],
-        cursos: [{
-            nomeDoCurso: "UX",
-            dataMatricula: new Date
+        cursos: [{ nomeDoCurso: "UX", dataMatricula: new Date
         }],
         faltas: 1
     }
@@ -67,8 +50,8 @@ const listarAlunos = () => {
 
 //Buscar por nome de aluno
 function buscarAluno(nome){
-    let result = alunosDaEscola.filter((elemento) => {
-        return elemento.nome.indexOf(nome) !== -1;
+    let result = alunosDaEscola.filter((aluno) => {
+        return aluno.nome.indexOf(nome) !== -1;
     });
     if (result.length >= 1) {
         console.log("Aluno " + nome + " encontrado:") 
@@ -78,8 +61,38 @@ function buscarAluno(nome){
     }   
 }
 
+//Cadastrar um aluno em um curso.
+function matricularAluno(aluno, curso){
+    let result = alunosDaEscola.filter((alunoAtual) => {
+        return alunoAtual.nome.indexOf(aluno.nome) !== -1;
+    });
+    if (result.length >= 1) {
+        aluno.cursos.push({nomeDoCurso:curso, dataMatricula: new Date});
+        return ".:.:.: Cadastro de Aluno em Curso :.:.:. \n" + 
+        "Aluno " + aluno.nome + " cadatrado no curso de: " + curso +" - Data " + new Date;
+    } else {
+        return ".:.:.: Cadastro de Aluno em Curso :.:.:. \n" + 
+        "Aluno não Cadastrado!"
+    } 
 
+    // alunosDaEscola.forEach(alunoAtual => {
+    //     if (alunoAtual.nome == aluno.nome) {
+    //         alunoAtual.cursos.push({nomeDoCurso:curso, dataMatricula: new Date}) 
+    //         console.log(`Aluno ${aluno.nome} cadatrado no curso de ${curso} - Data ${new Date}`); 
+    //         // `${alunoAtual.nome} cadastrado no curso(s) de ${curso} - Data ${alunoAtual.cursos.dataMatricula}`;
+    //     } else if (alunoAtual.nome !== aluno.nome){
+    //         console.log(`Aluno _${aluno.nome}_ não encontrado`);
+    //     }
+    // })
+}
 
 //console.log(adicionarAluno("Deyvison"));
 //console.log(listarAlunos());
 //console.log(buscarAluno("Henrique"))
+
+// let teste = {
+//     nome: "Joaoxi", notas: [], cursos: [], faltas: 2
+// }
+// console.log(matricularAluno(teste, "Java"));
+// console.log(matricularAluno(alunosDaEscola[1], "Java"));
+// console.log(listarAlunos());
